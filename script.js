3dbcfc0a42fa74039787618caf2d16feabd6013f
Silvia Taliana <silviataliana2@gmail.com>
2021-01-05 16:15:45 +1030

    added end page for entering initials

--- script.js
+++ script.js
@@ -102,144 +102,83 @@ function showQuestions(index) {
     // query selectors call on the questions and answer buttons
     var queText = document.querySelector(".queText");
     var optionList = document.querySelector(".optionList");
+    // Q&A's taken from the array
     var queTag = '<span>' + questions[index].question + '</span>';
     var optionTag = '<button class="btn btn-primary  answerBtn">' + questions[index].options[0] + '</button>'
         + '<button class="btn btn-primary answerBtn">' + questions[index].options[1] + '</button>'
         + '<button class="btn btn-primary answerBtn">' + questions[index].options[2] + '</button>'
         + '<button class="btn btn-primary answerBtn">' + questions[index].options[3] + '</button>';
+    // Q&A's shown on the screen 
     queText.innerHTML = queTag;
     optionList.innerHTML = optionTag;
 
+    // function for when answer button is clicked by user
     document.querySelectorAll(".answerBtn").forEach(function (button) {
         button.addEventListener("click", function () {
+            // when the user selects the correct answer
             if (button.innerHTML === questions[index].answer) {
                 score++;
                 console.log(score);
                 alert("correct");
             }
+            // when the user selects the incorrect answer
             else {
                 alert("incorrect");
                 // minus time
             }
             queCount++;
-            showQuestions(queCount);
-            console.log(button.innerHTML);
-            console.log(questions[index].answer);
-            // when button clicked - ?right/wrong
-            // if wrong, minus time
-            // if correct, add score
-            // move to next question queCount
-            // showQuestion()
-        })
+            // loop to see when the user gets to the last question 
+            if (queCount < questions.length) {
+                showQuestions(queCount);
+                console.log(queCount);
+            }
+            else {
+                // if it is the last question, the showInitials function starts
+                entrInitials();
+            }
+        });
     });
-
-
-    // for (var i = 0; i < questions.length; i++) {
-    //     var response = questions[i].options;
-    //     if (response == questions[i].answer) {
-    //         score++;
-    //         alert("Correct!");
-    //     } else {
-    //         alert("Incorrect");
-    //     }
-    // }
-    // alert("You got " + score + "/" + questions.length);
-    // console.log(response)
 }
 
-// function showQuestions(index) {
-//     for (var i = 0; i < questions.length; i++) {
-//         var response = window.questions(questions[i].question);
-//         if (response == questions[i].answer) {
-//             score++;
-//             alert("Correct!");
-//         } else {
-//             alert("Incorrect");
-//         }
-//     }
-// }
+// user enters their initials here, quiz is hidden, timer stops 
+function entrInitials(event) {
+    // var initials = document.getElementById("#enterInitials");
+    // var enterBtn = document.getElementById("#enterBtn");
+    // addEventListener("click", function(){
 
-/*
-// getting questions and answers from array
-function showQuestions(index) {
-    var queText = document.querySelector(".queText");
-    var optionList = document.querySelector(".optionList");
-    let queTag = '<span>' + questions[index].question + '</span>';
-    let optionTag = '<div class="optionList">' + questions[index].options[0] + '<label></label></div>'
-        + '<div class="optionList">' + questions[index].options[1] + '<span></span></div>'
-        + '<div class="optionList">' + questions[index].options[2] + '<span></span></div>'
-        + '<div class="optionList">' + questions[index].options[3] + '<span></span></div>';
-    queText.innerHTML = queTag;
-    optionList.innerHTML = optionTag;
+    // })
+    var endPage = document.getElementById("end").style.display = "block";
+    quizBox.style.display = "none";
+    alert("You got " + score + "/" + questions.length);
 }
-*/
 
+const initials = document.getElementById("enterInitials");
+const enterBtn = document.getElementById("enterBtn");
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+// finalScore.innerText = mostRecentScore;
 
+// initials.addEventListener("keyup", () => {
+//     enterBtn.disabled = !initials.value;
+// });
 
+saveHighScore = (e) => {
+    e.preventDefault();
+    console.log("save button clicked")
 
-// var questions = [
-//     {
-//         prompt: "first javascript question?\n(a) one\n\(b) two\n\(c) three\n\(d) four",
-//         answer: "a"
-//     },
-//     {
-//         prompt: "second javascript question?\n(a) one\n\(b) two\n\(c) three\n\(d) four",
-//         answer: "c"
-//     },
-//     {
-//         prompt: "third javascript question?\n(a) one\n\(b) two\n\(c) three\n\(d) four",
-//         answer: "a"
-//     },
-//     {
-//         prompt: "fourth javascript question?\n(a) one\n\(b) two\n\(c) three\n\(d) four",
-//         answer: "d"
-//     },
-//     {
-//         prompt: "fith javascript question?\n(a) one\n\(b) two\n\(c) three\n\(d) four",
-//         answer: "b"
-//     }
-// ]
-// var score = 0;
+    const score = {
+        score: mostRecentScore,
+        name: initials.value,
+    };
+}
+/*   highScores.push(score);
+   highScores.sort((a, b) => b.score - a.score);
 
-// for (var i = 0; i < questions.length; i++) {
-//     var response = window.prompt(questions[i].prompt);
-//     if (response == questions[i].answer) {
-//         score++;
-//         alert("Correct!");
-//     } else {
-//         alert("Incorrect");
-//     }
-// }
-// alert("You got " + score + "/" + questions.length);
+   localStorage.setItem('highScores', JSON.stringify(highScores));
+   window.location.assign('/');
 
-
-
-/* psuedo code
-starts at main page
-1. when user presses start button, first question appears and
-timer starts counting down
-2. user answers question 1, if correct: correct appears below,
-a point is added to their score (stored) and second question appears.
-if incorrect: incorrect appears below,
-timer deducts 10 seconds and second question appears.
-    NOTE: incorrect answers can equal 0 and correct answers can equal 1
-    so then the var sum = all answers added together = final score
-    can possibly declare sum = a + b + c etc. where a = first answer etc.
-    OR could use a function??
-    global variables
-    var answer;
-    var correct;
-    var incorrect;
-    function score() {
-        var finalScore = " "
-        if (answer = "correct")
-        finalScore++;
-    }
-3. step 2 repeats until all questions are answered OR timer
-reaches 0
-4. when either of the steps in 3 is reached, the "All done"
-page appears and a box appears where the user can insert their
-initials, which are stored alongside their score.
-5. the highscore page appears and every user who has played the
-quiz is listed according to their score. */
+   console.log(highScores);
+};
+console.log(initials); */
